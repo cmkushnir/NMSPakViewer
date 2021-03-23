80bbc5602b0ea188529067468e1220693e01ddad
cmkushnir <cmkushnir@outlook.com>
2021-03-23 15:50:02 -0400

    Update AssemblyInfo

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -7,12 +7,12 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle( "NMSS" )]
+[assembly: AssemblyTitle( "NMSPakViewer" )]
 [assembly: AssemblyDescription( "" )]
 [assembly: AssemblyConfiguration( "" )]
 [assembly: AssemblyCompany( "" )]
 [assembly: AssemblyProduct( "NMSS" )]
-[assembly: AssemblyCopyright( "Copyright ©  2021" )]
+[assembly: AssemblyCopyright("Copyright © 2021 Chris Kushnir")]
 [assembly: AssemblyTrademark( "" )]
 [assembly: AssemblyCulture( "" )]
 
